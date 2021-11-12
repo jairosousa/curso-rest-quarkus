3d66d746bb1dc669d94e4c65808f73c39f32394b
Jairo <jaironsousa@gmail.com>
2021-11-12 11:03:15 -0300

    :construction: Mapeamento da Entidade de Users

---
+++ quarkus-social/src/main/java/io/github/jairosousa/quarkussocial/dpmain/model/User.java
@@ -0,0 +1,58 @@
+package io.github.jairosousa.quarkussocial.dpmain.model;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 12/11/2021 - 10:48
+ */
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name = "name")
+    private String name;
+    @Column(name = "age")
+    private Integer age;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        User user = (User) o;
+        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, age);
+    }
+}
