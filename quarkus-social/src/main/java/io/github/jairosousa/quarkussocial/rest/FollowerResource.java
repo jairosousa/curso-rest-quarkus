3ee0b3c8c666570080144fe9b94dde6256a1e662
Jairo <jaironsousa@gmail.com>
2021-11-14 13:48:33 -0300

    :construction: Implementando o método de seguir um usuário

--- quarkus-social/src/main/java/io/github/jairosousa/quarkussocial/rest/FollowerResource.java
+++ quarkus-social/src/main/java/io/github/jairosousa/quarkussocial/rest/FollowerResource.java
@@ -1,21 +1,15 @@
 package io.github.jairosousa.quarkussocial.rest;
 
-import io.github.jairosousa.quarkussocial.domain.model.Post;
+import io.github.jairosousa.quarkussocial.domain.model.Follower;
 import io.github.jairosousa.quarkussocial.domain.model.User;
 import io.github.jairosousa.quarkussocial.domain.repository.FollowerRepository;
-import io.github.jairosousa.quarkussocial.domain.repository.PostRepository;
 import io.github.jairosousa.quarkussocial.domain.repository.UserRepository;
-import io.github.jairosousa.quarkussocial.rest.dto.CreatePostRequest;
-import io.github.jairosousa.quarkussocial.rest.dto.PostResponse;
-import io.quarkus.hibernate.orm.panache.PanacheQuery;
-import io.quarkus.panache.common.Sort;
+import io.github.jairosousa.quarkussocial.rest.dto.FollowerRequest;
 
 import javax.inject.Inject;
-import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.util.stream.Collectors;
 
 /**
  * @Autor Jairo Nascimento
@@ -35,5 +29,20 @@ public class FollowerResource {
         this.followerRepository = followerRepository;
     }
 
-    
+    @PUT
+    public Response followerUser(@PathParam("userId") Long userId, FollowerRequest request) {
+        var user = userRepository.findById(userId);
+        if (user == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        var follower = userRepository.findById(request.getFollowerId());
+
+        var entity = new Follower();
+        entity.setUser(user);
+        entity.setFollower(follower);
+
+        followerRepository.persist(entity);
+
+        return Response.status(Response.Status.NO_CONTENT).build();
+    }
 }
---
+++ quarkus-social/src/main/java/io/github/jairosousa/quarkussocial/rest/dto/FollowerRequest.java
@@ -0,0 +1,13 @@
+package io.github.jairosousa.quarkussocial.rest.dto;
+
+import lombok.Data;
+
+/**
+ * @Autor Jairo Nascimento
+ * @Created 14/11/2021 - 13:42
+ */
+@Data
+public class FollowerRequest {
+
+    private Long followerId;
+}
