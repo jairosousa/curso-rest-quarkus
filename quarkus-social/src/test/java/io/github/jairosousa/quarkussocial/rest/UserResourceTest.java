0a4ed808b00a8bbf654f655977a91291edec7790
Jairo <jaironsousa@gmail.com>
2021-11-18 18:08:06 -0300

    :white_check_mark: Testando o caso de erro de validação.

--- quarkus-social/src/test/java/io/github/jairosousa/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/jairosousa/quarkussocial/rest/UserResourceTest.java
@@ -1,10 +1,14 @@
 package io.github.jairosousa.quarkussocial.rest;
 
 import io.github.jairosousa.quarkussocial.rest.dto.CreateUserRequest;
+import io.github.jairosousa.quarkussocial.rest.dto.ResponseError;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.Map;
 
 import static io.restassured.RestAssured.given;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -34,6 +38,31 @@ class UserResourceTest {
 
         assertEquals(201, response.statusCode());
         assertNotNull(response.jsonPath().getString("id"));
+    }
+
+    @Test
+    @DisplayName("should return error when json is not valid")
+    public void createValidationTest() {
+        var user = new CreateUserRequest();
+        user.setAge(null);
+        user.setName("Fulano");
+
+        var response = given()
+                .contentType(ContentType.JSON)
+                .body(user)
+                .when()
+                .post("/users")
+                .then()
+                .extract().response();
+
+        assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
+        assertEquals("Validation Error",response.jsonPath().getString("message"));
+
+        List<Map<String, String>> errors = response.jsonPath().getList("errors");
+
+        assertNotNull(errors.get(0).get("message"));
+        assertEquals("Age is required",errors.get(0).get("message"));
+
     }
 
 }
