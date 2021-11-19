4aeab5c670653d5e84076204013d918958d231fa
Jairo <jaironsousa@gmail.com>
2021-11-19 14:09:06 -0300

    :white_check_mark: Realizando testes ordenados.

--- quarkus-social/src/test/java/io/github/jairosousa/quarkussocial/rest/UserResourceTest.java
+++ quarkus-social/src/test/java/io/github/jairosousa/quarkussocial/rest/UserResourceTest.java
@@ -2,11 +2,13 @@ package io.github.jairosousa.quarkussocial.rest;
 
 import io.github.jairosousa.quarkussocial.rest.dto.CreateUserRequest;
 import io.github.jairosousa.quarkussocial.rest.dto.ResponseError;
+import io.quarkus.test.common.http.TestHTTPResource;
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.*;
 
+import java.net.URL;
 import java.util.List;
 import java.util.Map;
 
@@ -19,10 +21,15 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
  * @Created 18/11/2021 - 15:43
  */
 @QuarkusTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class UserResourceTest {
+
+    @TestHTTPResource("/users")
+    URL apiURL;
 
     @Test
     @DisplayName("should create an user successfully")
+    @Order(1)
     public void createUserTest() {
         var user = new CreateUserRequest();
         user.setAge(30);
@@ -32,7 +39,7 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(user)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract().response();
 
@@ -42,6 +49,7 @@ class UserResourceTest {
 
     @Test
     @DisplayName("should return error when json is not valid")
+    @Order(2)
     public void createValidationTest() {
         var user = new CreateUserRequest();
         user.setAge(null);
@@ -51,18 +59,31 @@ class UserResourceTest {
                 .contentType(ContentType.JSON)
                 .body(user)
                 .when()
-                .post("/users")
+                .post(apiURL)
                 .then()
                 .extract().response();
 
         assertEquals(ResponseError.UNPROCESSABLE_ENTITY_STATUS, response.statusCode());
-        assertEquals("Validation Error",response.jsonPath().getString("message"));
+        assertEquals("Validation Error", response.jsonPath().getString("message"));
 
         List<Map<String, String>> errors = response.jsonPath().getList("errors");
 
         assertNotNull(errors.get(0).get("message"));
-        assertEquals("Age is required",errors.get(0).get("message"));
+        assertEquals("Age is required", errors.get(0).get("message"));
 
+    }
+
+    @Test
+    @DisplayName("should list all users")
+    @Order(3)
+    public void listAllUsersTest() {
+        given()
+                .contentType(ContentType.JSON)
+                .when()
+                .get(apiURL)
+                .then()
+                .statusCode(200)
+                .body("size()", Matchers.is(1));
     }
 
 }
